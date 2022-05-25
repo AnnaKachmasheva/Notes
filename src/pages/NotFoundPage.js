70d23846d653dc22055b8f7bb5fac6cdf8059203
kachmann <Disciples1>
2022-05-25 14:32:29 +0200

    fixed error page style

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -3,7 +3,7 @@ import Footer from "../parts/FooterPart";
 
 const NoteFound = () => (
     <div>
-        <div className="error-container'">
+        <div className="error-container">
             <h1>Stránka nenalezena</h1>
             <h2>Error 404</h2>
             <a href='/'>Jít na domovskou stránku</a>
